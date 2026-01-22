4fdb0c7885bfde21e7cfb766639bb3b416c21cd9
junior <lesageekane@gmail.com>
2026-01-22 01:28:14 +0100

    initial commit

---
+++ src/main/java/com/jekdev/com/saapp/SaAppApplication.java
@@ -0,0 +1,13 @@
+package com.jekdev.com.saapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jekdev/com/saapp/SaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jekdev.com.saapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
