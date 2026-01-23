7b12a4d344674f9842975a7c2cd7e56bc7d745c2
junior <lesageekane@gmail.com>
2026-01-24 00:37:06 +0100

    Make Client readable
    - Remove the test controller
    - Add unique property to client entity to avoid double clients
    - Provide client and emotion entities a constructor managed by spring container
    - Create a ClientController to receive client request
    - Create a ClientService to handle ClientController request like search one or more clients and also create clients
    - Add a method in ClientRepository to find client by email and check if a client already exists

--- src/main/java/com/jekdev/com/entities/Emotion.java
+++ src/main/java/com/jekdev/com/entities/Emotion.java
@@ -7,12 +7,16 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Emotion {
 
     @Id
