72dfabdf19bde1a2518458becf00c5d9dac64d9b
junior <lesageekane@gmail.com>
2026-01-23 01:03:46 +0100

    Create initial project structure
    - Define entities and repositories
    - Add a portable database in a docker-compose file with admin ui
    - Add devtools dependency to reload app by changes
    - Move SaAppApplication from package saapp to base path to make component scan easier
    - Add a basic RestController with a test path and endpoint

---
+++ src/main/java/com/jekdev/com/sacontroller/SaController.java
@@ -0,0 +1,15 @@
+package com.jekdev.com.sacontroller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "test")
+public class SaController {
+
+    @GetMapping(path = "hello")
+    public String getString(){
+        return "Hello\n";
+    }
+}
