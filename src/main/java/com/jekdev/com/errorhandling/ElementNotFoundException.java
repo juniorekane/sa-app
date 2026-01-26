a21c06cfa40018ab3e698469bef2de9cf4615c43
junior <lesageekane@gmail.com>
2026-01-26 23:07:43 +0100

    Update doc and return correct http status

--- src/main/java/com/jekdev/com/errorhandling/ElementNotFoundException.java
+++ src/main/java/com/jekdev/com/errorhandling/ElementNotFoundException.java
@@ -1,25 +1,20 @@
 package com.jekdev.com.errorhandling;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
- * Exception thrown when the requested element cannot be found within the application.
+ * Exception thrown when a requested element cannot be found in the application.
  *
- * <p>This exception is typically used to signal that a specific resource or data entity was not found, resulting in a
- * 404 Not Found HTTP response status. It serves as a mechanism to communicate such errors in a standardized manner
- * across the application.
+ * <p>This exception signals that an attempt to fetch or access a specific element has failed because the element does
+ * not exist. It is typically used in scenarios where the application cannot locate the requested data due to its
+ * absence. As a runtime exception, it allows developers to handle missing elements contextually in the application.
  *
- * <p>The exception is associated with Spring's {@code @ResponseStatus} annotation, which automatically maps the
- * exception to an HTTP response with the status {@code HttpStatus.NOT_FOUND}.
+ * <p>For example, this exception corresponds to cases where an invalid query or operation is attempted for a
+ * non-existent element, and it is commonly used in conjunction with a global exception handling mechanism. In the
+ * context of a centralized exception handler, such as {@code GlobalExceptionHandler}, this exception can be mapped to
+ * return a standardized error response with an HTTP 400 Bad Request status.
  *
- * <p>For global handling of this exception and to ensure consistent error responses, refer to the {@code
- * GlobalExceptionHandler} class, where it is mapped to produce a standardized error message in a JSON format.
- *
- * @see ResponseStatus
  * @see GlobalExceptionHandler
  */
-@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ElementNotFoundException extends RuntimeException {
   public ElementNotFoundException(String message) {
     super(message);
