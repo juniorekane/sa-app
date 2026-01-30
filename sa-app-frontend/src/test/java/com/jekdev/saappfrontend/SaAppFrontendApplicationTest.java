62c3f4db0b520fb9430a44274de2fc63ae1345d9
junior <lesageekane@gmail.com>
2026-01-30 04:20:17 +0100

    add main class in frontend

---
+++ sa-app-frontend/src/test/java/com/jekdev/saappfrontend/SaAppFrontendApplicationTest.java
@@ -0,0 +1,11 @@
+package com.jekdev.saappfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaAppFrontendApplicationTest {
+
+  @Test
+  void contextLoad() {}
+}
